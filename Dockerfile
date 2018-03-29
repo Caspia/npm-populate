5653224a198ca010fe7e8fa87e79cc5ad32af0ad
R. Kent James <kent@caspia.com>
2018-03-28 17:49:48 -0700

    Support a start.sh usable in rancheros (unstable)

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,5 @@
 FROM verdaccio/verdaccio:2.7.4
 
-ARG CONFIGFILE
-ENV CONFIGFILE ${CONFIGFILE:-config.yaml}
-ARG PORT
-ENV PORT ${PORT:-4873}
-ENV PROTOCOL http
-ENV APPDIR /usr/local/app
-EXPOSE ${PORT}
-
 # Adapted from ope-verdaccio from the ope project
 
 MAINTAINER R Kent James <rkent@caspia.org>
@@ -15,6 +7,4 @@ LABEL Description="NPM offline repository demo using verdaccio" Version="2.7.4"
 COPY ./config.yaml /verdaccio/conf/config.yaml
 COPY ./configOnline.yaml /verdaccio/conf/configOnline.yaml
 
-
 VOLUME ["/verdaccio/storage"]
-CMD $APPDIR/bin/verdaccio --config /verdaccio/conf/$CONFIGFILE --listen $PROTOCOL://0.0.0.0:${PORT}
---
+++ release-laptop/start.sh
@@ -0,0 +1,10 @@
+. ./.env
+docker kill nginx-proxy
+docker rm nginx-proxy
+docker run --name nginx-proxy -d --network=nginx-proxy -p 80:80 --restart=always --env-file .env -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy:alpine
+docker kill dnsmasq-proxy
+docker rm dnsmasq-proxy
+docker run --name dnsmasq-proxy -d --network=nginx-proxy -p 53:53/tcp -p 53:53/udp --restart=always -e DOMAIN=$DOMAIN -e IP_ADDR=$IP_ADDR --cap-add NET_ADMIN rkent/dnsmasq:proxy -A /$DOMAIN/$IP_ADDR
+docker kill verdaccio
+docker rm verdaccio
+docker run --name verdaccio -d --network=nginx-proxy -p 4873:4873 --env-file .env --restart=always -v /srv/verdaccio:/verdaccio/storage:rw verdaccio:caspia /usr/local/app/bin/verdaccio --config /verdaccio/conf/$CONFIGFILE --listen http://0.0.0.0:4873
